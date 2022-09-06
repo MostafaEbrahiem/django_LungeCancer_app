19890d2ca8c41062630974f1bf03709a61e7a211
Mostafa <mostafaebrahiem1998@gmail.com>
2022-09-06 17:24:31 +0200

    modified:   LungeCancer/__pycache__/settings.cpython-310.pyc 	modified:   LungeCancer/settings.py

--- LungeCancer/settings.py
+++ LungeCancer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 django_heroku.settings(locals())
 
